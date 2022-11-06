ec32df7976a560ff7a84521b5230ffd53bca925c
IgorEverton <rm95214@fiap.com.br>
2022-11-06 12:42:49 -0300

    colocando as duas telas no App

--- eletech/src/App.jsx
+++ eletech/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import Cadastro_fisica from './cadastro/Cadastro_fisica';
+import Cadastro_juridica from './cadastro/Cadastro_juridica';
 
 // import { Container } from './styles';
 
 export default function App() {
-  return <div />;
+  return <div>
+
+            <Cadastro_fisica/>
+            <Cadastro_juridica/>;
+
+            </div>
 }
 
